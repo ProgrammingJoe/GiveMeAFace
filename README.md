83709bf8df598ca942a02f498e97eec5bca20ce0
Joe <joeczepil@gmail.com>
2019-01-09 21:52:21 -0800

    Basic project setup and a couple ideas/plans

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # GiveMeAFace
+
+### Setup
+
+* Clone the repo
+* Install virtualenv
+* `virtualenv --python=/usr/bin/python3.6 venv`
+* `pip install -r requirements.txt`
---
+++ makeaface.py
@@ -0,0 +1,51 @@
+import gizeh
+
+## Possible configurations to code
+# Face style
+# Number of faces returned
+# Resolution(s)
+# Round or Square background
+
+## Ideas
+# https://dribbble.com/search?q=profile+icon
+
+# Utility Functions
+def getRandomColor():
+    pass
+
+def getRandomPattern():
+    pass
+
+# Generate Functions
+def generateEyes():
+    # random shape
+    # random colors
+    # random patterns
+    pass
+
+def generateFace():
+    # random shape
+    # random colors
+    # random patterns
+    pass
+
+def generateMouth():
+    # random shape
+    # random colors
+    # random patterns
+    pass
+
+def generateBody():
+    # random shape
+    # random colors
+    # random patterns
+    pass
+
+def main():
+    # initialize surface
+    surface = gizeh.Surface(width=300, height=300)
+    print('in main')
+
+
+if __name__ == '__main__':
+    main()
